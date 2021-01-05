c2d363c934fe11ace1f310a883c6ad050f5474b7
ThanhChung <ntchung18041999@gmail.com>
2021-01-05 17:49:32 +0700

    create Voucher update responsive Banner

--- src/App/index.js
+++ src/App/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './layouts/Header'
 import Banner from './layouts/Banner'
+import Voucer from './layouts/Voucher'
+import Voucher from './layouts/Voucher';
 
 function HomePage() {
   return (
     <div className="home">
       <Header />
       <Banner />
+      <Voucher />
     </div>
   );
 }
---
+++ src/App/layouts/Voucher.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Container } from 'reactstrap';
+
+import { ReactComponent as Plane} from "../../assets/icons/plane.svg"
+import { ReactComponent as Gift} from "../../assets/icons/gift.svg"
+import { ReactComponent as Star} from "../../assets/icons/star.svg"
+
+
+function Voucher() {
+    return (
+        <Container className="voucher">
+            <div className="voucher__detail">
+                <Plane className="voucher__detail-icon" />
+                <p className="voucher__detail-desc">Miễn phí vận chuyển</p>
+            </div>
+            <div className="voucher__detail">
+                <Gift className="voucher__detail-icon" />
+                <p className="voucher__detail-desc">Nhận ngay quà tặng trị giá 350.000đ</p>
+            </div>
+            <div className="voucher__detail">
+                <Star className="voucher__detail-icon" />
+                <p className="voucher__detail-desc">Giảm 30% cho đơn hàng trên 5.000.000đ</p>
+            </div>
+        </Container>
+    )
+}
+
+export default Voucher
