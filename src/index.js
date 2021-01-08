db411b2be7633b166f33ca3fbba1e6a0921fc2a6
ThanhChung <ntchung18041999@gmail.com>
2021-01-08 16:23:01 +0700

    test 6

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../src/App/index';
 import reportWebVitals from './reportWebVitals';
-
-import './styles/reset.scss'
-import './styles/base/base.scss'
 import './styles/styles.scss'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
